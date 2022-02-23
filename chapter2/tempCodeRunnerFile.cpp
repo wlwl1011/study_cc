f110f99436960956acbcc8cdf5d39ef4313001ea
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2022-02-23 14:00:10 +0900

    study about true and flase

---
+++ chapter2/tempCodeRunnerFile.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 10;
+    int i = 0;
+
+    cout << "true: " << true << endl;
+    cout << "false: " << false << endl;
+
+    while (true)
+    {
+        cout << i++ << ' ';
+        if (i > num)
+        {
+            break;
+        }
+        cout << endl;
+    }
+
+    cout << "sizeof 1: " << sizeof(1) << endl;
+    cout << "sizeof 0: " << sizeof(0) << endl;
+    cout << "sizeof ture: " << sizeof(true) << endl;
+    cout << "sizeof flase: " << sizeof(false) << endl;
+
+    return 0;
+}
